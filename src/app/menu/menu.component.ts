f848b622b33f3c0e9dbf336dfcf7a1d83c99e247
cedric maroka <cedric.maroka@mtn.com>
2021-01-24 16:44:14 +0200

    first commit

---
+++ src/app/menu/menu.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { DishService } from '../services/dish.service';
+import { Dish } from '../shared/dish';
+import { DISHES } from '../shared/dishes';
+
+@Component({
+  selector: 'app-menu',
+  templateUrl: './menu.component.html',
+  styleUrls: ['./menu.component.scss']
+})
+export class MenuComponent implements OnInit {
+  dishes: Dish[] = DISHES;
+
+  selectedDish: Dish;
+
+  constructor(private dishService: DishService) { }
+
+  ngOnInit(): void {
+    this.dishes = this.dishService.getDishes();
+  }
+
+  onSelect(dish: Dish) {
+    this.selectedDish = dish;
+  }
+
+}
